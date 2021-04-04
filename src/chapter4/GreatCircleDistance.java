65e5350f3513fde0d78a0a21e90d469dba577c00
Steve Palko <63173670+srpalko@users.noreply.github.com>
2021-04-04 19:21:17 -0400

    Move GreatCircleDistance.java logic into a public method

--- src/chapter4/GreatCircleDistance.java
+++ src/chapter4/GreatCircleDistance.java
@@ -14,21 +14,32 @@ public class GreatCircleDistance
         // Get input from user
         Scanner input = new Scanner(System.in);
         System.out.print("Enter point 1 (latitude and longitude) in degrees separated by a space: ");
-        double latitude1 = Math.toRadians(input.nextDouble() );
-        double longitude1 = Math.toRadians(input.nextDouble() );
+        double latitude1 = Math.toRadians(input.nextDouble());
+        double longitude1 = Math.toRadians(input.nextDouble());
         System.out.print("Enter point 2 (latitude and longitude) in degrees separated by a space: ");
-        double latitude2 = Math.toRadians(input.nextDouble() );
-        double longitude2 = Math.toRadians(input.nextDouble() );
+        double latitude2 = Math.toRadians(input.nextDouble());
+        double longitude2 = Math.toRadians(input.nextDouble());
 
         // Calculate result
-        final double AVG_EARTH_RADIUS_KM = 6371.01;
-        double distance = AVG_EARTH_RADIUS_KM * Math.acos(Math.sin(latitude1) * Math.sin(latitude2)
-                + Math.cos(latitude1) * Math.cos(latitude2) * Math.cos(longitude1 - longitude2) );
+        double distance = findGreatCircleDistance(latitude1, longitude1, latitude2, longitude2);
 
         // Display result
         System.out.println("The distance between the two points is " + distance + " km");
+    }
 
-
-
+    /**
+     * Find the great circle distance between two point on the earth.
+     *
+     * @param x1 Latitude of first point in degrees
+     * @param y1 Longitude of first point in degrees
+     * @param x2 Latitude of second point in degrees
+     * @param y2 Longitude of second point in degrees
+     * @return distance between the two points in km
+     */
+    public static double findGreatCircleDistance(double x1, double y1, double x2, double y2)
+    {
+        final double AVG_EARTH_RADIUS_KM = 6371.01;
+        return AVG_EARTH_RADIUS_KM * Math.acos(Math.sin(x1) * Math.sin(x2)
+                + Math.cos(x1) * Math.cos(x2) * Math.cos(y1 - y2));
     }
 }
