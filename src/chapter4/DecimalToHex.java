73c56fd6908c9df67f10e5270861b71d9236bff3
Steve Palko <63173670+srpalko@users.noreply.github.com>
2021-04-05 07:06:12 -0400

    Add DecimalToHex.java

---
+++ src/chapter4/DecimalToHex.java
@@ -0,0 +1,29 @@
+package chapter4;
+
+import java.util.Scanner;
+
+/**
+ * Converts a decimal number to its
+ * corresponding hex value between 0 and 15.
+ */
+public class DecimalToHex
+{
+    public static void main(String[] args)
+    {
+        Scanner input = new Scanner(System.in);
+        System.out.print("Enter a decimal value (0 to 15): ");
+        int number = input.nextInt();
+
+        if (number > 15 || number < 0)
+        {
+            System.out.println(number + " is an invalid input");
+        } else if (number > 9)
+        {
+            int hex = number + 55;
+            System.out.println("The hex value is " + (char) hex);
+        } else
+        {
+            System.out.println("The hex value is " + number);
+        }
+    }
+}
