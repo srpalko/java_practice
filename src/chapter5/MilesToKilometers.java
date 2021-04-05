b41434e44da7fcc44583d0977ef43727ac081398
Steve Palko <63173670+srpalko@users.noreply.github.com>
2021-04-05 16:50:23 -0400

    Add MilesToKilometers

---
+++ src/chapter5/MilesToKilometers.java
@@ -0,0 +1,20 @@
+package chapter5;
+
+/**
+ * Display a table of miles 1, 2, ..., 9, 10 to kilometers
+ */
+public class MilesToKilometers
+{
+    public static void main(String[] args)
+    {
+        final double MILES_TO_KILOMETERS = 1.60934;
+
+        System.out.println("Miles     Kilometers");
+
+        for (int miles = 1; miles <= 10; miles++)
+        {
+            double km = miles * MILES_TO_KILOMETERS;
+            System.out.printf("%-5d     %.3f\n", miles, km);
+        }
+    }
+}
