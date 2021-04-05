7a524bc1088e76484748fb3b61212b09af49c368
Steve Palko <63173670+srpalko@users.noreply.github.com>
2021-04-04 20:24:55 -0400

    Add AreaOfHexagon

---
+++ src/chapter4/AreaOfHexagon.java
@@ -0,0 +1,20 @@
+package chapter4;
+
+import java.util.Scanner;
+
+/**
+ * Displays the area of a hexagon based on the length of a side.
+ */
+public class AreaOfHexagon
+{
+    public static void main(String[] args)
+    {
+        Scanner input = new Scanner(System.in);
+        System.out.print("Enter the side: ");
+        double side = input.nextDouble();
+
+        double area = (6 * side * side) / (4 * Math.tan(Math.PI / 6));
+
+        System.out.printf("The area of the hexagon is %.2f", area);
+    }
+}
