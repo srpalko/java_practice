ab6d6165e9a04689aea6cd8923a9104fb346f6e0
Steve Palko <63173670+srpalko@users.noreply.github.com>
2021-04-06 09:41:48 -0400

    Add DisplayASCIITable

---
+++ src/chapter5/DisplayASCIITable.java
@@ -0,0 +1,21 @@
+package chapter5;
+
+/**
+ * Displays the ASCII character table from ! to ~.
+ */
+public class DisplayASCIITable
+{
+    public static void main(String[] args)
+    {
+        int count = 0;
+        for (char c = '!'; c <= '~'; c++)
+        {
+            System.out.print(c + " ");
+            count++;
+            if (count % 10 == 0)
+            {
+                System.out.println();
+            }
+        }
+    }
+}
