a200941898f75acb55b6cf74e005d8f40998e0f5
Steve Palko <63173670+srpalko@users.noreply.github.com>
2021-04-06 11:10:58 -0400

    Add PatternC

---
+++ src/chapter5/PatternC.java
@@ -0,0 +1,23 @@
+package chapter5;
+
+public class PatternC
+{
+    public static void main(String[] args)
+    {
+        System.out.println("Pattern C");
+        for (int i = 1; i <= 6; i++)
+        {
+            for (int j = 6; j >= 1; j--)
+            {
+                if (j <= i)
+                {
+                    System.out.printf("%2d", j);
+                } else
+                {
+                    System.out.print("  ");
+                }
+            }
+            System.out.println();
+        }
+    }
+}
