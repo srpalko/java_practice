7c3c0003870867ce6a6f7c4d531c1d9dc04f8549
Steve Palko <63173670+srpalko@users.noreply.github.com>
2021-04-06 20:45:42 -0400

    Add ComputePi

---
+++ src/chapter5/ComputePi.java
@@ -0,0 +1,27 @@
+package chapter5;
+
+/**
+ * Approximates pi
+ */
+public class ComputePi
+{
+    public static void main(String[] args)
+    {
+        for (int j = 10000; j <= 100000 ; j+=10000)
+        {
+            double sum = 0;
+            for (int i = 1, count = 1; i < (2 * j - 1); i += 2, count++)
+            {
+                if (count % 2 != 0)
+                {
+                    sum += 1.0 / i;
+                } else
+                {
+                    sum -= 1.0 / i;
+                }
+            }
+            double pi = 4 * sum;
+            System.out.println("With i = " + j + ", the approximation of pi is " + pi);
+        }
+    }
+}
