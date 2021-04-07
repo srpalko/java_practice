d5d2f5c4ea1c2fb5cfbf44021f507816a164c7f1
Steve Palko <63173670+srpalko@users.noreply.github.com>
2021-04-07 10:54:07 -0400

    Add DecimalToBinary.java

---
+++ src/chapter5/DecimalToBinary.java
@@ -0,0 +1,25 @@
+package chapter5;
+
+import java.util.Scanner;
+
+/**
+ * Converts a decimal integer to binary the hard way.
+ */
+public class DecimalToBinary
+{
+    public static void main(String[] args)
+    {
+        Scanner input = new Scanner(System.in);
+        System.out.print("Enter an integer: ");
+        int number = input.nextInt();
+        StringBuilder binary = new StringBuilder();
+
+        while (number > 0)
+        {
+            binary.append(number % 2);
+            number /= 2;
+        }
+        binary.reverse();
+        System.out.println(binary);
+    }
+}
