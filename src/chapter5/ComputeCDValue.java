f2ccc0e020f5420f5352ca94dd124bed990055d9
Steve Palko <63173670+srpalko@users.noreply.github.com>
2021-04-07 00:31:31 -0400

    Add ComputeCDValue.java

---
+++ src/chapter5/ComputeCDValue.java
@@ -0,0 +1,28 @@
+package chapter5;
+
+import java.util.Scanner;
+
+/**
+ * Takes an amount, the annual percentage yield, and the number of months
+ * and displays a table of CD values per month.
+ */
+public class ComputeCDValue
+{
+    public static void main(String[] args)
+    {
+        Scanner input = new Scanner(System.in);
+        System.out.print("Enter the initial deposit amount: ");
+        double amount = input.nextDouble();
+        System.out.print("Enter the annual percentage yield: ");
+        double yield = input.nextDouble();
+        System.out.print("Enter the maturity period (number of months): ");
+        int months = input.nextInt();
+        double balance = 10000;
+        System.out.println("Month    CD Value");
+        for (int month = 1; month <= months; month++)
+        {
+            balance = balance + balance * yield / 1200;
+            System.out.printf("%-5d    $%.2f\n", month, balance);
+        }
+    }
+}
