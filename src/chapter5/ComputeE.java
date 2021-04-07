9b9e37dcebef4fdfd338de7a2cd74dccc16473d8
Steve Palko <63173670+srpalko@users.noreply.github.com>
2021-04-06 20:58:02 -0400

    Add ComputeE

---
+++ src/chapter5/ComputeE.java
@@ -0,0 +1,23 @@
+package chapter5;
+
+/**
+ * Approximates e
+ */
+public class ComputeE
+{
+    public static void main(String[] args)
+    {
+        for (int i = 10000; i <= 100000; i += 10000)
+        {
+            double e = 1;
+            double item = 1;
+            for (int j = 2; j <= i; j++)
+            {
+                e += item;
+                item = item / j;
+            }
+            System.out.println("With i = " + i + ", the approximation of e is " + e);
+        }
+        System.out.println("The value of e is " + Math.E);
+    }
+}
