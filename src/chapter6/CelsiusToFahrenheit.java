7645fb7690a74504283f4f5e53e52ac091c5ce29
Steve Palko <63173670+srpalko@users.noreply.github.com>
2021-04-08 13:16:37 -0400

    Add CelsiusToFahrenheit.java

---
+++ src/chapter6/CelsiusToFahrenheit.java
@@ -0,0 +1,36 @@
+package chapter6;
+
+public class CelsiusToFahrenheit
+{
+    public static void main(String[] args)
+    {
+        System.out.println("Celsius        Fahrenheit        |        Fahrenheit        Celsius");
+        System.out.println("-------------------------------------------------------------------");
+        for (int c = 40, f = 120; c >= 31; c--, f -= 10)
+        {
+            double fahrConversion = celsiusToFahrenheit(c);
+            double celsConversion = fahrenheitToCelsius(f);
+            System.out.printf("%-7.1f        %10.1f        |        %-10.1f        %7.2f\n", c * 1.0, fahrConversion, f * 1.0, celsConversion);
+        }
+    }
+
+    /**
+     * Converts degrees Celsius to degrees Fahrenheit
+     * @param celsius degrees Celsius
+     * @return degrees Fahrenheit
+     */
+    public static double celsiusToFahrenheit(double celsius)
+    {
+        return (9.0 / 5) * celsius + 32;
+    }
+
+    /**
+     * Converts degrees Fahrenheit to degrees Celsius
+     * @param fahrenheit degrees Fahrenheit
+     * @return degrees Celsius
+     */
+    public static double fahrenheitToCelsius(double fahrenheit)
+    {
+        return (5.0 / 9) * (fahrenheit - 32);
+    }
+}
