da2f061b19a129f6eb3ac581e10fed0ab174c3eb
Steve Palko <63173670+srpalko@users.noreply.github.com>
2021-04-11 20:03:50 -0400

    Add EliminateDuplicates.java

--- src/chapter7_1d_arrays/EliminateDuplicates.java
+++ src/chapter7_1d_arrays/EliminateDuplicates.java
@@ -1,5 +1,66 @@
 package chapter7_1d_arrays;
 
+import java.util.Arrays;
+import java.util.Scanner;
+
+/**
+ * Reads ten integers, invokes the method, and displays the result.
+ */
 public class EliminateDuplicates
 {
+    public static void main(String[] args)
+    {
+
+        Scanner input = new Scanner(System.in);
+        System.out.print("Enter ten numbers: ");
+        int[] entry = new int[10];
+        for (int i = 0, entryLength = entry.length; i < entryLength; i++)
+        {
+            entry[i] = input.nextInt();
+        }
+
+        int[] nonDupe = eliminateDuplicates(entry);
+        System.out.print("The distinct numbers are: ");
+        for (int num :
+                nonDupe)
+        {
+            System.out.print(num + " ");
+        }
+
+    }
+
+    /**
+     * Returns a new array by eliminating the duplicate values in the array.
+     * @param list array to be processed
+     * @return array with duplicates removed
+     */
+    public static int[] eliminateDuplicates(int[] list)
+    {
+        int[] result = new int[list.length];
+        int next = 0;
+        for (int num : list)
+        {
+            boolean inList = false;
+            for (int j : result)
+            {
+                if (j == num)
+                {
+                    inList = true;
+                    break;
+                }
+
+            }
+            if (!inList)
+            {
+                result[next] = num;
+                next++;
+            }
+        }
+        int[] trimmed = new int[next];
+        for (int i = 0, trimmedLength = trimmed.length; i < trimmedLength; i++)
+        {
+            trimmed[i] = result[i];
+        }
+        return trimmed;
+    }
 }
