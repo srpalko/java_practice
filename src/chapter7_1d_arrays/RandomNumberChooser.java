0a859266eb761dc4d9743bcb37ef50c070a0a9f6
Steve Palko <63173670+srpalko@users.noreply.github.com>
2021-04-13 13:50:02 -0400

    Add PickFourCards.java

--- src/chapter7_1d_arrays/RandomNumberChooser.java
+++ src/chapter7_1d_arrays/RandomNumberChooser.java
@@ -5,7 +5,7 @@ public class RandomNumberChooser
 {
     public static void main(String[] args)
     {
-        System.out.println(getRandom(1, 2));
+        System.out.println(getRandom(1, 54, 1, 2));
     }
 
     /**
@@ -14,13 +14,13 @@ public class RandomNumberChooser
      * @param numbers numbers to exclude from the random choice
      * @return a random integer between 1 and 54 inclusive. Excluding integers passed as arguments
      */
-    public static int getRandom(int... numbers)
+    public static int getRandom(int lowerBound, int upperBound, int... numbers)
     {
         boolean numberInArray;
         int randomNum;
         do
         {
-            randomNum = (int) (1 + Math.random() * 54);
+            randomNum = (int) (lowerBound + Math.random() * upperBound);
             numberInArray = false;
             for (int number : numbers)
             {
