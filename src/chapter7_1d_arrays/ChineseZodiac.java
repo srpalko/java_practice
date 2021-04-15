ee62a9e18c9db67702369e332ba820421eb615fd
Steve Palko <63173670+srpalko@users.noreply.github.com>
2021-04-15 09:52:28 -0400

    ChineseZodiac

---
+++ src/chapter7_1d_arrays/ChineseZodiac.java
@@ -0,0 +1,25 @@
+package chapter7_1d_arrays;
+
+import java.util.Scanner;
+
+/**
+ * Simplified version of class from chapter3 using an array of strings
+ */
+public class ChineseZodiac
+{
+    public static void main(String[] args)
+    {
+        Scanner input = new Scanner(System.in);
+
+        System.out.print("Enter a year: ");
+        int year = input.nextInt();
+
+        String[] zodiacNames = {"monkey", "rooster", "dog", "pig", "rat", "ox", "tiger", "rabbit", "dragon", "snake",
+                "horse", "sheep"};
+
+        int yearCycle = year % 12;
+
+        System.out.println(year + " is a year of the " + zodiacNames[yearCycle]);
+
+    }
+}
